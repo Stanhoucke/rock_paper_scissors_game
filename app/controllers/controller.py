b7f639d7e2af179cb8889a03cfdff0588fd4f9f2
Stanley Houcke <stanhoucke@gmail.com>
2020-12-04 18:37:51 +0000

    finished mvp

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -11,4 +11,5 @@ def index():
 
 @app.route("/<player_1_choice>/<player_2_choice>")
 def play_game(player_1_choice, player_2_choice):
-    return game.play(player_1_choice, player_2_choice)
+    result = game.play(player_1_choice, player_2_choice)
+    return render_template("result.html", player_1_choice = player_1_choice, player_2_choice = player_2_choice, result = result)
