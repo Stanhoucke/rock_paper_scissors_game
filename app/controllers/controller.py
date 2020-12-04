284a2fa27d7187a1cf25f083eb053abfb7787dda
Stanley Houcke <stanhoucke@gmail.com>
2020-12-04 18:26:23 +0000

    web app returns result of play method

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -3,7 +3,12 @@ from app import app
 from app.classes.player import Player
 from app.classes.game import Game
 
+game = Game()
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/<player_1_choice>/<player_2_choice>")
+def play_game(player_1_choice, player_2_choice):
+    return game.play(player_1_choice, player_2_choice)
