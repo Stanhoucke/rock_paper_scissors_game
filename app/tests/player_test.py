3d2802d73722181b985fd46d065dd73352244121
Stanley Houcke <stanhoucke@gmail.com>
2020-12-04 14:18:33 +0000

    set up player class and passed attribute tests

--- app/tests/player_test.py
+++ app/tests/player_test.py
@@ -2,4 +2,22 @@ import unittest
 from app.classes.player import Player
 
 class TestPlayer(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.player_1 = Player("David")
+        self.player_2 = Player("Emma")
+
+    def test_player_has_name(self):
+        self.assertEqual("David", self.player_1.name)
+
+    def test_player_choice__returns_rock(self):
+        choice = self.player_1.choice[0]
+        self.assertEqual("Rock", choice)
+
+    def test_player_choice__returns_paper(self):
+        choice = self.player_1.choice[1]
+        self.assertEqual("Paper", choice)
+
+    def test_player_choice__returns_scissors(self):
+        choice = self.player_1.choice[2]
+        self.assertEqual("Scissors", choice)
