1c629b64ca5c7063af8806fb48d666e2915d99cb
Stanley Houcke <stanhoucke@gmail.com>
2020-12-04 17:36:55 +0000

    passed test for rejecting invalid input

--- app/classes/game.py
+++ app/classes/game.py
@@ -1,13 +1,26 @@
 class Game():
     
     def play(self, player_1_choice, player_2_choice):
-        if player_1_choice == "Rock" and player_2_choice == "Scissors":
-            return f"Player 1 wins by playing {player_1_choice.lower()}!"
-        elif player_1_choice == "Paper" and player_2_choice == "Rock":
-            return f"Player 1 wins by playing {player_1_choice.lower()}!"
-        elif player_1_choice == "Scissors" and player_2_choice == "Paper":
-            return f"Player 1 wins by playing {player_1_choice.lower()}!"
-        elif player_1_choice == player_2_choice:
-            return "It's a tie!"
+        player_choice_list = [player_1_choice, player_2_choice]
+        valid_choice_list = ["Rock", "Paper", "Scissors"]
+        valid_choice_count = 0
+
+        # Check if both players input valid options
+        for player_choice in player_choice_list:
+            for valid_choice in valid_choice_list:
+                if player_choice == valid_choice:
+                    valid_choice_count += 1
+        if valid_choice_count != 2:
+            return "One of the players didn't choose 'Rock', 'Paper', or 'Scissors', please check your spelling and try again."
+        # Check for winner
         else:
-            return f"Player 2 wins by playing {player_2_choice.lower()}!"
+            if player_1_choice == "Rock" and player_2_choice == "Scissors":
+                return f"Player 1 wins by playing {player_1_choice.lower()}!"
+            elif player_1_choice == "Paper" and player_2_choice == "Rock":
+                return f"Player 1 wins by playing {player_1_choice.lower()}!"
+            elif player_1_choice == "Scissors" and player_2_choice == "Paper":
+                return f"Player 1 wins by playing {player_1_choice.lower()}!"
+            elif player_1_choice == player_2_choice:
+                return "It's a tie!"
+            else:
+                return f"Player 2 wins by playing {player_2_choice.lower()}!"
--- app/tests/game_test.py
+++ app/tests/game_test.py
@@ -31,3 +31,9 @@ class TestGame(unittest.TestCase):
 
     def test_play__paper_loses_to_scissors(self):
         self.assertEqual("Player 2 wins by playing scissors!", self.game.play(self.player_2.choice, self.player_3.choice))
+
+    def test_play__invalid_player_choice(self):
+        # Assign invalid choice to player
+        self.player_1.choice = "Lizard"
+        message = "One of the players didn't choose 'Rock', 'Paper', or 'Scissors', please check your spelling and try again."
+        self.assertEqual(message, self.game.play(self.player_1.choice, self.player_2.choice))
