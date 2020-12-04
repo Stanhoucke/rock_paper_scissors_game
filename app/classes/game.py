e1b2d262831b6e8a7e3db0ffd8cb44547cd0ee38
Stanley Houcke <stanhoucke@gmail.com>
2020-12-04 17:06:25 +0000

    passed tests for player 1 wins

--- app/classes/game.py
+++ app/classes/game.py
@@ -3,3 +3,11 @@ class Game():
     def play(self, player_1_choice, player_2_choice):
         if player_1_choice == "Rock" and player_2_choice == "Scissors":
             return f"Player 1 wins by playing {player_1_choice.lower()}!"
+        elif player_1_choice == "Paper" and player_2_choice == "Rock":
+            return f"Player 1 wins by playing {player_1_choice.lower()}!"
+        elif player_1_choice == "Scissors" and player_2_choice == "Paper":
+            return f"Player 1 wins by playing {player_1_choice.lower()}!"
+        elif player_1_choice == player_2_choice:
+            return "It's a tie!"
+        else:
+            return f"Player 2 wins by playing {player_2_choice.lower()}!"
--- app/tests/game_test.py
+++ app/tests/game_test.py
@@ -13,3 +13,9 @@ class TestGame(unittest.TestCase):
 
     def test_play__rock_beats_scissors(self):
         self.assertEqual("Player 1 wins by playing rock!", self.game.play(self.player_1.choice, self.player_3.choice))
+
+    def test_play__paper_beats_rock(self):
+        self.assertEqual("Player 1 wins by playing paper!", self.game.play(self.player_2.choice, self.player_1.choice))
+
+    def test_play__scissors_beat_paper(self):
+        self.assertEqual("Player 1 wins by playing scissors!", self.game.play(self.player_3.choice, self.player_2.choice))
