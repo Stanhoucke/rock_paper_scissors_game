58bf3ba5fce22b041132ecfad0628820579b5ce6
Stanley Houcke <stanhoucke@gmail.com>
2020-12-05 12:57:01 +0000

    web app allows for play against computer

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -13,3 +13,15 @@ def index():
 def play_game(player_1_choice, player_2_choice):
     result = game.play(player_1_choice, player_2_choice)
     return render_template("result.html", player_1_choice = player_1_choice, player_2_choice = player_2_choice, result = result)
+
+@app.route("/play", methods=["GET", "POST"])
+def play_computer():
+
+    if request.method == "GET":
+        return render_template("play.html")
+
+    else:
+        player_1 = Player(request.form["name"], request.form["choice"])
+        computer_choice = game.computer_choice()
+        result = game.play(player_1.choice, computer_choice)
+        return render_template("result.html", player_1_choice = player_1.choice, player_2_choice = computer_choice, result = result)
